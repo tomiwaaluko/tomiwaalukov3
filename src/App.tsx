48ab0b582e21e699eafaf5ecfdb9f2180c09ac40
Tomiwa <tomiwaaluko02@gmail.com>
2026-05-12 03:46:36 -0400

    vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -118,6 +119,7 @@ function App() {
           </Router>
         </MusicProvider>
       </TronThemeProvider>
+      <Analytics />
     </ThemeProvider>
   );
 }
